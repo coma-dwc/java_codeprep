c12e1567720e744c3d0cb623f6f19c187f8c0312
coma-dwc <tamagomapon@gmail.com>
2023-07-24 23:20:05 +0900

    first commit

---
+++ src/java_chapter1/Main.java
@@ -0,0 +1,9 @@
+package java_chapter1;
+
+//初めてのJavaアプリケーション　クラスを定義する
+public class Main {
+	public static void main(String[] args) {
+		//System.out println("Hello Error");
+		System.out.println("Hello World");
+	}
+}
