9cc168afe99a3bf501fca7f2b81fccf8fff78f7b
coma-dwc <tamagomapon@gmail.com>
2023-07-24 23:36:14 +0900

    chapter2 finish

---
+++ src/java_chapter2/Main.java
@@ -0,0 +1,13 @@
+package java_chapter2;
+
+//計算を行ってみる
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(5 + 2); 
+		System.out.println(5 - 2); 
+		System.out.println(5 * 2); 
+		System.out.println(5 / 2);
+		System.out.println(5 % 2);
+		System.out.println(5.0 / 2); //割り算(小数)
+	}
+}
