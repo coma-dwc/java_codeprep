2ede9e62191c92cf123d686be1c47394f4102297
coma-dwc <tamagomapon@gmail.com>
2023-07-25 23:30:17 +0900

    chapter5 finish

---
+++ src/java_chapter5/Main.java
@@ -0,0 +1,80 @@
+package java_chapter5;
+
+//「論理演算」「比較演算」を行う
+//論理AND演算子(&&)
+
+public class Main {
+//	public static void main(String[] args) {
+//		System.out.println(true && true);
+//		
+//		System.out.println(true && false);
+//		System.out.println(false && false);
+//	}
+	
+	//論理OR演算子(||)
+//	public static void main(String[] args) {
+//		System.out.println(true || false);
+//		System.out.println(false || true);
+//		
+//		System.out.println(false || false);
+//	}
+	
+	//論理NOT演算子(!)
+//	public static void main(String[] args) {
+//		System.out.println(!true);
+//		
+//		System.out.println(!false);
+//	}
+	
+	//等価演算子(==)
+//	public static void main(String[] args) {
+//		System.out.println(10 == 10);
+//		
+//		System.out.println(10 == 0);
+//	}
+	//※Javaでは==で等価比較が行えるのはintやbooleanなどの基本データ型のみで、
+	//Stringなどの基本データ型以外で比較を行う場合はequalsメソッドを使用する必要がある
+	//例：System.out.println("Hello".equals("Hello"));// trueが表示されます。
+	
+	//不等価演算子(!=)
+//	public static void main(String[] args) {
+//		System.out.println(10 != 10);
+//		
+//		System.out.println(10 != 0);
+//	}
+	//※Stringなどの基本データ型以外で不等価演算を行う場合は
+	//equalsメソッドと否定演算子を組み合わせる
+	//例：System.out.println(!"Hello".equals("Hello"));// falseが表示されます
+
+	//関係演算子(>)
+//	public static void main(String[] args) {
+//		System.out.println(10 > 1);
+//		
+//		System.out.println(1 > 10);
+//	}
+	
+	//関係演算子(>=)
+//	public static void main(String[] args) {
+//		System.out.println(10 >= 1);
+//		
+//		System.out.println(10 >= 10);
+//		
+//		System.out.println(1 >= 10);
+//	}
+	
+	//関係演算子(<)
+//	public static void main(String[] args) {
+//		System.out.println(10 < 1);
+//		
+//		System.out.println(1 < 10);
+//	}
+	
+	//関係演算子(<=)
+//	public static void main(String[] args) {
+//		System.out.println(10 <= 1);
+//		
+//		System.out.println(10 <= 10);
+//		
+//		System.out.println(1 <= 10);
+//	}
+}
